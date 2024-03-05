f75fb364168a02f21b4b6f3076a7c1988c70aaf8
Derrick Ampire <ampire90@gmail.com>
2024-03-05 20:57:18 +0300

    indentation fixed

--- OrderBookEntry.cpp
+++ OrderBookEntry.cpp
@@ -1,10 +1,10 @@
 #include "OrderBookEntry.h"
 
 OrderBookEntry::OrderBookEntry (double _price,
-								double _amount,
-								std::string _timestamp,
-								std::string _product,
-								OrderBookType _orderType)
+				double _amount,
+				std::string _timestamp,
+				std::string _product,
+				OrderBookType _orderType)
 : price(_price),
   amount (_amount),
   timestamp (_timestamp),
