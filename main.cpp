0daf622168ccbfb11524731053653e81372fe7c8
Derrick Ampire <ampire90@gmail.com>
2024-03-05 09:43:40 +0300

    class removed and added to header file

--- main.cpp
+++ main.cpp
@@ -2,41 +2,7 @@
 #include <map>
 #include <string>
 #include <vector>
-
-enum class OrderBookType{ask, bid};
-
-class OrderBookEntry
-{
-	public:
-		/** Create a new OrderBookEntry with the price
-		* set to the sent value
-		*/
-		OrderBookEntry( double _price,
-				double _amount,
-				std::string _timestamp,
-				std::string _product,
-				OrderBookType _orderType);
-		
-		double price;
-		double amount;
-		std::string timestamp;
-		std::string product;
-		OrderBookType orderType;
-};
-
-OrderBookEntry::OrderBookEntry (double _price,
-				double _amount,
-				std::string _timestamp,
-				std::string _product,
-				OrderBookType _orderType)
-: price(_price),
-  amount (_amount),
-  timestamp (_timestamp),
-  product (_product),
-  orderType (_orderType)
-{
-
-}
+#include "OrderBookEntry.h"
 
 void printMenu()
 {
