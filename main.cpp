5bdb3a48c3e27addc588ff587ec076a063495be0
Derrick Ampire <ampire90@gmail.com>
2024-03-05 12:14:48 +0300

    compute prices functions moved to their specific file

--- main.cpp
+++ main.cpp
@@ -1,76 +1,16 @@
 #include <iostream>
-#include <map>
 #include <string>
 #include <vector>
 #include "OrderBookEntry.h"
 #include "MerkelMain.h"
+#include "ComputePrices.h"
 
 
-double computeAveragePrice(std::vector<OrderBookEntry>& orders)
-{
-	double sum = 0.0;
-	for (OrderBookEntry& order : orders)
-	{
-		sum += order.price;
-	}
-
-	return sum / orders.size();
-}
-
-
-double computeLowPrice(std::vector<OrderBookEntry>& orders)
-{
-	double lowest = orders[0].price;
-	for (OrderBookEntry& order : orders)
-	{
-		if (order.price < lowest)
-		{
-			lowest = order.price;
-		}
-	}
-
-	return lowest;
-}
-
-
-double computeHighPrice(std::vector<OrderBookEntry>& orders)
-{
-	double highest = orders[0].price;
-	for (OrderBookEntry& order : orders)
-	{
-		if (order.price > highest)
-		{
-			highest = order.price;
-		}
-	}
-
-	return highest;
-}
-
-
-double computePriceSpread(std::vector<OrderBookEntry>& orders)
-{
-	double min = orders[0].price;
-	double max = orders[0].price;
-
-	for (OrderBookEntry& order : orders)
-	{
-		if (order.price < min)
-		{
-			min = order.price;
-		}
-		if (order.price > max)
-		{
-			max = order.price;
-		}
-	}
-
-	return max - min;
-}
 
 int main()
 {
 	MerkelMain app{};
+	ComputePrices compute{};
 	
 	std::vector<OrderBookEntry> orders;
 
@@ -103,10 +43,10 @@ int main()
 		std::cout << order.price << std::endl;
 	}
 
-	std::cout << "The average price is " << computeAveragePrice(orders) << std::endl;
-	std::cout << "The lowest price is " << computeLowPrice(orders) << std::endl;
-	std::cout << "The highest price is " << computeHighPrice(orders) << std::endl;
-	std::cout << "The price spread is " << computePriceSpread(orders) << std::endl;
+	std::cout << "The average price is " << compute.computeAveragePrice(orders) << std::endl;
+	std::cout << "The lowest price is " << compute.computeLowPrice(orders) << std::endl;
+	std::cout << "The highest price is " << compute.computeHighPrice(orders) << std::endl;
+	std::cout << "The price spread is " << compute.computePriceSpread(orders) << std::endl;
 	std::cout << std::endl;
 
 	while (true)
@@ -115,34 +55,6 @@ int main()
 		int userOption = app.getUserOption();
 		app.processUserOption(userOption);
 	}
-
-	
-	/*
-	std::map<int, void(*)()> menu;
-	menu[1] = printHelp;
-	menu[2] = printMarketStats;
-	menu[3] = enterOffer;
-	menu[4] = enterBid;
-	menu[5] = printWallet;
-	menu[6] = gotoNextTimeframe;
-
-	while (true)
-	{
-		printMenu();
-		int userOption = getUserOption();
-
-		auto i = menu.find(userOption);
-		if (i != menu.end())
-		{
-			i->second();
-		}
-		else
-		{
-			std::cout << "Invalid choice. Choose an option between 1-6" << std::endl; 
-		}
-	}
-	*/
-
 
 	return 0;
 }
