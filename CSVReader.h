8330f1abbe9dc2ad81c7800228544b8fac2cbe27
Derrick Ampire <ampire90@gmail.com>
2024-03-25 09:34:19 +0300

    tokenize function made public, strings to Order Book Entry function added

--- CSVReader.h
+++ CSVReader.h
@@ -15,8 +15,13 @@ class CSVReader
         * 2020/03/17 17:01:24.884492,ETH/BTC,bid,0.02187305,6.85567013
         */
         static std::vector<OrderBookEntry> readCSV(std::string csvFile);
+        static std::vector<std::string> tokenize(std::string csvLine, char separator);
+        static OrderBookEntry stringsToOBE(std::string price,
+                                            std::string amount,
+                                            std::string timestamp,
+                                            std::string product,
+                                            std::string OrderBookType);
 
     private:
-        static std::vector<std::string> tokenize(std::string csvLine, char separator);
         static OrderBookEntry stringsToOBE(std::vector<std::string> strings);
 };
